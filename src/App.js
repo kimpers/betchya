2847201f2ff6048392ce0666f0e386007db58e9e
Kim Persson <kimpersson88@gmail.com>
2018-07-18 21:08:54 +0200

    ADDS prettier and linting

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import React, { Component } from 'react'
-import SimpleStorageContract from '../build/contracts/SimpleStorage.json'
-import getWeb3 from './utils/getWeb3'
-import { Header } from 'semantic-ui-react'
+import React, { Component } from "react";
+import SimpleStorageContract from "../build/contracts/SimpleStorage.json";
+import getWeb3 from "./utils/getWeb3";
+import { Header } from "semantic-ui-react";
 
 class App extends Component {
   constructor(props) {
-    super(props)
+    super(props);
 
     this.state = {
       storageValue: 0,
       web3: null
-    }
+    };
   }
 
   componentWillMount() {
@@ -18,17 +18,17 @@ class App extends Component {
     // See utils/getWeb3 for more info.
 
     getWeb3
-    .then(results => {
-      this.setState({
-        web3: results.web3
-      })
+      .then(results => {
+        this.setState({
+          web3: results.web3
+        });
 
-      // Instantiate contract once web3 provided.
-      this.instantiateContract()
-    })
-    .catch(() => {
-      console.log('Error finding web3.')
-    })
+        // Instantiate contract once web3 provided.
+        this.instantiateContract();
+      })
+      .catch(() => {
+        console.log("Error finding web3.");
+      });
   }
 
   instantiateContract() {
@@ -39,35 +39,41 @@ class App extends Component {
      * state management library, but for convenience I've placed them here.
      */
 
-    const contract = require('truffle-contract')
-    const simpleStorage = contract(SimpleStorageContract)
-    simpleStorage.setProvider(this.state.web3.currentProvider)
+    const contract = require("truffle-contract");
+    const simpleStorage = contract(SimpleStorageContract);
+    simpleStorage.setProvider(this.state.web3.currentProvider);
 
     // Declaring this for later so we can chain functions on SimpleStorage.
-    var simpleStorageInstance
+    var simpleStorageInstance;
 
     // Get accounts.
     this.state.web3.eth.getAccounts((error, accounts) => {
-      simpleStorage.deployed().then((instance) => {
-        simpleStorageInstance = instance
+      simpleStorage
+        .deployed()
+        .then(instance => {
+          simpleStorageInstance = instance;
 
-        // Stores a given value, 5 by default.
-        return simpleStorageInstance.set(5, {from: accounts[0]})
-      }).then((result) => {
-        // Get the value from the contract to prove it worked.
-        return simpleStorageInstance.get.call(accounts[0])
-      }).then((result) => {
-        // Update state with the result.
-        return this.setState({ storageValue: result.c[0] })
-      })
-    })
+          // Stores a given value, 5 by default.
+          return simpleStorageInstance.set(5, { from: accounts[0] });
+        })
+        .then(result => {
+          // Get the value from the contract to prove it worked.
+          return simpleStorageInstance.get.call(accounts[0]);
+        })
+        .then(result => {
+          // Update state with the result.
+          return this.setState({ storageValue: result.c[0] });
+        });
+    });
   }
 
   render() {
     return (
       <div className="App">
         <nav className="navbar pure-menu pure-menu-horizontal">
-            <a href="#" className="pure-menu-heading pure-menu-link">Truffle Box</a>
+          <a href="#" className="pure-menu-heading pure-menu-link">
+            Truffle Box
+          </a>
         </nav>
         <Header size="huge">HEADER</Header>
 
@@ -77,8 +83,14 @@ class App extends Component {
               <h1>Good to Go!</h1>
               <p>Your Truffle Box is installed and ready.</p>
               <h2>Smart Contract Example</h2>
-              <p>If your contracts compiled and migrated successfully, below will show a stored value of 5 (by default).</p>
-              <p>Try changing the value stored on <strong>line 59</strong> of App.js.</p>
+              <p>
+                If your contracts compiled and migrated successfully, below will
+                show a stored value of 5 (by default).
+              </p>
+              <p>
+                Try changing the value stored on <strong>line 59</strong> of
+                App.js.
+              </p>
               <p>The stored value is: {this.state.storageValue}</p>
             </div>
           </div>
@@ -88,4 +100,4 @@ class App extends Component {
   }
 }
 
-export default App
+export default App;
