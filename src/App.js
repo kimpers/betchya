40339a8008bb69d47fa56bf9d2e588351ffecae9
Kim Persson <kimpersson88@gmail.com>
2018-07-22 22:23:44 +0200

    ADDS connect react bet form with createBet contract method

--- src/App.js
+++ src/App.js
@@ -2,18 +2,23 @@ import React, { Component } from "react";
 import BetchyaContract from "../build/contracts/Betchya.json";
 import getWeb3 from "./utils/getWeb3";
 import styled from "styled-components";
-import { Header } from "semantic-ui-react";
+import { Header, Message } from "semantic-ui-react";
 
 import BetForm from "./components/BetForm";
 
 const AppWrapper = styled.div`
+  height: 100%;
+  width: 100%;
+  background: #a1d2ce;
+`;
+
+const ContentWrapper = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
   height: 100%;
   width: 100%;
-  background: #a1d2ce;
 `;
 
 const AppHeader = styled(Header)`
@@ -27,7 +32,9 @@ class App extends Component {
 
     this.state = {
       storageValue: 0,
-      web3: null
+      web3: null,
+      contractMethods: null,
+      message: null
     };
   }
 
@@ -49,39 +56,69 @@ class App extends Component {
       });
   }
 
-  instantiateContract() {
+  handleEvent = (err, eventLog) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+
+    if (eventLog.event === "BetCreated") {
+      this.setState({
+        message:
+          "Bet successfully created! Awaiting acceptor and judge to confirm."
+      });
+    }
+  };
+
+  instantiateContract = () => {
     const contract = require("truffle-contract");
     const betchya = contract(BetchyaContract);
     betchya.setProvider(this.state.web3.currentProvider);
 
-    // Declaring this for later so we can chain functions on SimpleStorage.
-    let betchyaInstance;
+    const { web3 } = this.state;
 
     // Get accounts.
-    this.state.web3.eth.getAccounts((error, accounts) => {
-      //betchya
-      //.deployed()
-      //.then(instance => {
-      //betchyaInstance = instance;
-      //// Stores a given value, 5 by default.
-      //return betchyaInstance.set(5, { from: accounts[0] });
-      //})
-      //.then(result => {
-      //// Get the value from the contract to prove it worked.
-      //return betchyaInstance.get.call(accounts[0]);
-      //})
-      //.then(result => {
-      //// Update state with the result.
-      //return this.setState({ storageValue: result.c[0] });
-      //});
+    web3.eth.getAccounts(async (error, accounts) => {
+      const betchyaInstance = await betchya.deployed();
+
+      betchyaInstance.allEvents({}, this.handleEvent);
+      const account = accounts[0];
+
+      const contractMethods = {
+        createBet: (acceptor, judge, value) =>
+          betchyaInstance.createBet(acceptor, judge, {
+            from: account,
+            value: (web3.utils || web3).toWei(value, "ether")
+          })
+      };
+
+      this.setState({
+        contractMethods
+      });
     });
-  }
+  };
 
   render() {
+    const { message } = this.state;
+
     return (
       <AppWrapper>
-        <AppHeader size="huge">Betchya.eth - challenge your friends!</AppHeader>
-        <BetForm />
+        {message && (
+          <Message
+            positive
+            header="Success"
+            onDismiss={() => this.setState({ message: null })}
+          >
+            {message}
+          </Message>
+        )}
+
+        <ContentWrapper>
+          <AppHeader size="huge">
+            Betchya.eth - challenge your friends!
+          </AppHeader>
+          <BetForm contractMethods={this.state.contractMethods} />
+        </ContentWrapper>
       </AppWrapper>
     );
   }
--- src/components/BetForm/index.js
+++ src/components/BetForm/index.js
@@ -6,28 +6,66 @@ const FormInput = styled(Input)`
   margin-bottom: 1em;
 `;
 
-const BetForm = () => (
-  <div
-    style={{
-      display: "flex",
-      flexDirection: "column",
-      maxWidth: "600px"
-    }}
-  >
-    <FormInput label="ETH address of the challenged" placeholder="0x123456" />
-    <FormInput label="ETH address of the judge" placeholder="0x123456" />
+class BetForm extends Component {
+  // TODO: remove default values
+  state = {
+    acceptorAddress: "0x3dc9310B010bAEC67cBFb9816DB86435DE057Bac",
+    judgeAddress: "0x5C2a6467d6a1ddaE6C736c5dF0FB5f1F63D0715A",
+    value: 0.1
+  };
 
-    <FormInput
-      action={{
-        color: "teal",
-        labelPosition: "left",
-        icon: "ethereum",
-        content: "Make bet"
-      }}
-      label="Amount to bet (in ether)"
-      placeholder="0.1"
-    />
-  </div>
-);
+  handleCreateBet = () => {
+    const { contractMethods } = this.props;
+    const { acceptorAddress, judgeAddress } = this.state;
+
+    if (contractMethods && acceptorAddress && judgeAddress) {
+      contractMethods.createBet(
+        acceptorAddress,
+        judgeAddress,
+        this.state.value
+      );
+    } else {
+      console.error(contractMethods, acceptorAddress, judgeAddress);
+    }
+  };
+
+  render() {
+    return (
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          maxWidth: "900px"
+        }}
+      >
+        <FormInput
+          label="ETH address of the challenged"
+          placeholder="0x123456"
+          defaultValue="0x3dc9310B010bAEC67cBFb9816DB86435DE057Bac"
+          onChange={e => this.setState({ acceptorAddress: e.target.value })}
+        />
+        <FormInput
+          label="ETH address of the judge"
+          placeholder="0x123456"
+          defaultValue="0x5C2a6467d6a1ddaE6C736c5dF0FB5f1F63D0715A"
+          onChange={e => this.setState({ judgeAddress: e.target.value })}
+        />
+
+        <FormInput
+          action={{
+            color: "teal",
+            labelPosition: "left",
+            icon: "ethereum",
+            content: "Make bet",
+            onClick: this.handleCreateBet
+          }}
+          label="Amount to bet (in ether)"
+          placeholder="0.1"
+          onChange={e => this.setState({ value: e.target.value })}
+        />
+      </div>
+    );
+  }
+}
 
 export default BetForm;
