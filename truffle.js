04346af035b538dbd9e3412fd7c08999b29bf43a
Kim Persson <kimpersson88@gmail.com>
2018-08-26 00:08:08 +0200

    ADDS configuration for deploying to Rinkeby with Infura

--- truffle.js
+++ truffle.js
@@ -1,10 +1,31 @@
 require("babel-register");
+const HDWalletProvider = require("truffle-hdwallet-provider");
+const mnemonic = process.env.MNEMONIC;
+const infuraKey = process.env.INFURA_KEY;
+
 module.exports = {
   networks: {
     development: {
       host: "localhost",
       port: 8545,
       network_id: "*" // Match any network id
+    },
+    rinkeby: {
+      provider: () => {
+        if (!mnemonic) {
+          throw new Error("MNEMONIC env variable missing");
+        }
+
+        if (!infuraKey) {
+          throw new Error("INFURA_KEY env variable missing");
+        }
+
+        return new HDWalletProvider(
+          mnemonic,
+          `https://rinkeby.infura.io/${infuraKey}`
+        );
+      },
+      network_id: 4
     }
   }
 };
